c597e5a9c6cf0e982800e9cb423bfe75e3c86ff2
ChristianMazzaro <christianmazzaro2@gmail.com>
2023-07-04 17:33:56 +0200

    Esercizio completato

---
+++ js/script.js
@@ -0,0 +1,62 @@
+
+
+const { createApp } = Vue;
+
+createApp({
+
+    data(){
+
+
+        return{   
+
+            newTodo:'',
+
+            toDos: [
+
+                {
+                    text: "rubare le pensioni alle anziane fuori dalle poste",
+                    done:true
+                },
+                {
+                    text: "fare di tutta l'erba un fascio;",
+                    done:false
+                },
+                {
+                    text: "mettere la cravatta alle uova in camicia",
+                    done:false
+                },
+                {
+                    text: "piangere sulò latte versato",
+                    done:false
+                }
+            ]
+        }   
+        
+    },
+
+    methods : {
+
+        pushNewTodo(){
+
+            if(this.newTodo != ''){
+            this.toDos.push({
+                text: this.newTodo,
+                done: false
+            });
+
+            this.newTodo= '';
+            }
+        },
+
+        removeDone(i){
+            console.log('remove todo', i)
+            this.toDos.splice(i, 1)
+        },
+
+
+
+    }
+
+}).mount('#app');
+
+
